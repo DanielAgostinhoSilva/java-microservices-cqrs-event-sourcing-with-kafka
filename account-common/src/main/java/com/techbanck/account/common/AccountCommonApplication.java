7c101159ada26f8895f744b2595d0b3bb92ac0b3
Daniel <daniel.agostinho@hotmail.com>
2022-01-26 20:45:45 -0300

    create account-common and cqrs-core

---
+++ account-common/src/main/java/com/techbanck/account/common/AccountCommonApplication.java
@@ -0,0 +1,13 @@
+package com.techbanck.account.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountCommonApplication.class, args);
+    }
+
+}
---
+++ cqrs-core/src/main/java/com/techbank/cqrs/core/CqrsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.techbank.cqrs.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsCoreApplication.class, args);
+    }
+
+}
