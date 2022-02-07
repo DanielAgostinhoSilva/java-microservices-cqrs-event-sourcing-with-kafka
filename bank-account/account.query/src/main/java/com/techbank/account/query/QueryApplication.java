13db6a29ef02f2cc0f6a6ace039c40f43a4d65f2
Daniel <daniel.agostinho@hotmail.com>
2022-02-07 19:10:13 -0300

    65. Register Query Handlers

--- bank-account/account.query/src/main/java/com/techbank/account/query/QueryApplication.java
+++ bank-account/account.query/src/main/java/com/techbank/account/query/QueryApplication.java
@@ -1,13 +1,31 @@
 package com.techbank.account.query;
 
+import com.techbank.account.query.api.queries.*;
+import com.techbank.cqrs.core.infrastructure.QueryDispatcher;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
+
+@AllArgsConstructor
 @SpringBootApplication
 public class QueryApplication {
 
+	private QueryDispatcher queryDispatcher;
+	private QueryHandler queryHandler;
+
 	public static void main(String[] args) {
 		SpringApplication.run(QueryApplication.class, args);
+	}
+
+	@PostConstruct
+	public void registerHandlers() {
+		queryDispatcher.registerHandler(FindAllAccountsQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByIdQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountByHolderQuery.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindAccountsByBalanceQuery.class, queryHandler::handle);
 	}
 
 }
--- bank-account/account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountByIdQuery.java
+++ bank-account/account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountByIdQuery.java
@@ -1,5 +1,6 @@
 package com.techbank.account.query.api.queries;
 
+import com.techbank.cqrs.core.queries.BaseQuery;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,6 +8,6 @@ import javax.management.Query;
 
 @Data
 @AllArgsConstructor
-public class FindAccountByIdQuery extends Query {
+public class FindAccountByIdQuery extends BaseQuery {
     private String id;
 }
